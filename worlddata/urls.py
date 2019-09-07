c80d95acab3a7de107a0c39ea2aad24270f6f6c9
Unknown <kelanved@gmail.com>
2019-09-07 14:12:13 +0300

    added more data pages, and implemented linking

--- worlddata/urls.py
+++ worlddata/urls.py
@@ -1,7 +1,11 @@
 from django.urls import include, path
-from .views import DeathView, IndexView
+from .views import BiodiversityView, DeathView, IndexView, MigrationView, SustainabilityView
 
+app_name = 'worlddata'
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
-    path('death', DeathView.as_view(), name='death')
+    path('death', DeathView.as_view(), name='death'),
+    path('sustainability', SustainabilityView.as_view(), name='sustainability'),
+    path('biodiversity', BiodiversityView.as_view(), name='biodiversity'),
+    path('migration', MigrationView.as_view(), name='migration')
 ]
--- worlddata/views.py
+++ worlddata/views.py
@@ -40,3 +40,71 @@ class DeathView(TemplateView):
             comment.page = self.page
             comment.save()
         return HttpResponseRedirect(self.request.path_info)
+
+
+class SustainabilityView(TemplateView):
+    template_name = 'worlddata/sustainability.html'
+    page = get_object_or_404(Page, title="sustainability")
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        comments = self.page.comments.all()
+        comment_form = CommentForm()
+        context = {
+            'comments': comments,
+            'comment_form': comment_form
+        }
+        return context
+
+    def post(self, request, *args, **kwargs):
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.page = self.page
+            comment.save()
+        return HttpResponseRedirect(self.request.path_info)
+
+
+class BiodiversityView(TemplateView):
+    template_name = 'worlddata/biodiversity.html'
+    page = get_object_or_404(Page, title="biodiversity")
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        comments = self.page.comments.all()
+        comment_form = CommentForm()
+        context = {
+            'comments': comments,
+            'comment_form': comment_form
+        }
+        return context
+
+    def post(self, request, *args, **kwargs):
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.page = self.page
+            comment.save()
+        return HttpResponseRedirect(self.request.path_info)
+
+class MigrationView(TemplateView):
+    template_name = 'worlddata/migration.html'
+    page = get_object_or_404(Page, title="migration")
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        comments = self.page.comments.all()
+        comment_form = CommentForm()
+        context = {
+            'comments': comments,
+            'comment_form': comment_form
+        }
+        return context
+
+    def post(self, request, *args, **kwargs):
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.page = self.page
+            comment.save()
+        return HttpResponseRedirect(self.request.path_info)
