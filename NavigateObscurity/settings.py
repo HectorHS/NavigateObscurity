23d6570d3fa7364a3b0a7d2977040131a78f841b
Unknown <kelanved@gmail.com>
2019-10-07 11:32:50 +0300

    added staticfiles folder

--- NavigateObscurity/settings.py
+++ NavigateObscurity/settings.py
@@ -13,7 +13,9 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+BASE_DIR = os.path.dirname(PROJECT_DIR)
 
 
 # Quick-start development settings - unsuitable for production
@@ -131,6 +133,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 # TODO: update on deployment
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
---
+++ staticfiles/main/js/function.js
@@ -0,0 +1,23 @@
+
+(function($) { 
+  $(function() {
+    // Show navbar dropdown 
+    $('nav ul li a:not(:only-child)').click(function(e) {
+      $(this).siblings('.nav-dropdown').toggle();
+      $('.dropdown').not($(this).siblings()).hide();
+      e.stopPropagation();
+    });
+    // // Collapse dropdowns
+    // $('html').click(function() {
+    //   $('.nav-dropdown').hide();
+    // });
+    // // Reveal mobile menu on click
+    $('#nav-toggle').click(function() {
+      $('nav ul').slideToggle();
+    });
+    // Animate hamburger icon on click
+    $('#nav-toggle').on('click', function() {
+      this.classList.toggle('active');
+    });
+  }); 
+})(jQuery);
