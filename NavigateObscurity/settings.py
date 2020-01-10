5ed5a2a6828b61a1cccc9b65d9c78d30538052ed
Unknown <kelanved@gmail.com>
2020-01-10 15:56:45 +0200

    some updates to settings for deployment

--- NavigateObscurity/settings.py
+++ NavigateObscurity/settings.py
@@ -22,12 +22,12 @@ BASE_DIR = os.path.dirname(PROJECT_DIR)
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.navigateobscurity.com']
 
 
 # Application definition
