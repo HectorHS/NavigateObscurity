d38c05daf7a0e1fdac316c61791dce7a6229f4e5
Unknown <kelanved@gmail.com>
2020-01-27 11:13:02 +0200

    changed mobile menu and added figure tags

--- main/static/main/js/function.js
+++ main/static/main/js/function.js
@@ -1,23 +1,20 @@
-
-(function($) { 
-  $(function() {
-    // Show navbar dropdown 
-    $('nav ul li a:not(:only-child)').click(function(e) {
-      $(this).siblings('.nav-dropdown').toggle();
-      $('.dropdown').not($(this).siblings()).hide();
-      e.stopPropagation();
+(function ($) {
+  $(function () {
+    // Reveal mobile menu on click
+    $('#nav-toggle').click(function () {
+      $('.nav-list-mobile').slideToggle();
     });
-    // // Collapse dropdowns
-    // $('html').click(function() {
-    //   $('.nav-dropdown').hide();
-    // });
-    // // Reveal mobile menu on click
-    $('#nav-toggle').click(function() {
-      $('nav ul').slideToggle();
+    // Reveal data dropdown on mobile
+    $('#data_toggle').click(function () {
+      $('.data-dropdown').slideToggle();
+    });
+    // Reveal Notes dropdown on mobile
+    $('#notes_toggle').click(function () {
+      $('.notes-dropdown').slideToggle();
     });
     // Animate hamburger icon on click
-    $('#nav-toggle').on('click', function() {
+    $('#nav-toggle').on('click', function () {
       this.classList.toggle('active');
     });
-  }); 
+  });
 })(jQuery);
