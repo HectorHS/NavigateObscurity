f3ddd355e3ac69f54f3339d02544b0026e719eb8
HectorHS <kelanved@gmail.com>
2022-03-16 09:59:02 +0200

    removed jquery dependency

--- static/main/js/function.js
+++ static/main/js/function.js
@@ -1,20 +1,56 @@
-(function ($) {
-  $(function () {
-    // Reveal mobile menu on click
-    $('#nav-toggle').click(function () {
-      $('.nav-list-mobile').slideToggle();
-    });
-    // Reveal data dropdown on mobile
-    $('#data_toggle').click(function () {
-      $('.data-dropdown').slideToggle();
-    });
-    // Reveal Notes dropdown on mobile
-    $('#notes_toggle').click(function () {
-      $('.notes-dropdown').slideToggle();
-    });
-    // Animate hamburger icon on click
-    $('#nav-toggle').on('click', function () {
-      this.classList.toggle('active');
-    });
-  });
-})(jQuery);
+let menuContainer = document.getElementById('nav_list_mobile');
+let hamContainer = document.getElementById('nav_hamburger');
+let dataContainer = document.getElementById('data_dropdown');
+let notesContainer = document.getElementById('notes_dropdown');
+
+// hide/show menu
+hamContainer.addEventListener('click', function (event) {
+  event.preventDefault();
+  slideMenu(menuContainer);
+});
+// hide/show data menu
+document.getElementById('data_toggle').addEventListener('click', function (event) {
+  event.preventDefault();
+  slideMenu(dataContainer);
+});
+
+// hide/show notes menu
+document.getElementById('notes_toggle').addEventListener('click', function (event) {
+  event.preventDefault();
+  slideMenu(notesContainer);
+});
+
+// hide/show function
+function slideMenu(container) {
+  if (!container.classList.contains('active')) {
+      container.classList.add('active');
+      container.style.height = 'auto';
+
+      let height = container.clientHeight + 'px';
+
+      container.style.height = '0px';
+
+      setTimeout(function () {
+          container.style.height = height;
+      }, 0);
+  } else {
+      container.style.height = '0px';
+
+      container.addEventListener('transitionend', function () {
+          container.classList.remove('active');
+      }, {
+          once: true
+      });
+  }
+}
+
+      // for mobile menu hamburger animation
+hamContainer.addEventListener('click', function (event) {
+    event.preventDefault();
+
+    if (!hamContainer.classList.contains('active')) {
+        hamContainer.classList.add('active');
+    } else {
+        hamContainer.classList.remove('active');
+    }
+});
