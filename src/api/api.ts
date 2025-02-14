d8a5ca93d3f5958157f61254249fc09bba75e9bc
HectorHS <kelanved@gmail.com>
2025-02-14 11:11:05 +0200

    http for api

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL:  'https://api.navigateobscurity.com'
+  baseURL:  'http://api.navigateobscurity.com'
   // baseURL:  'http://localhost:5000'
 });
 
